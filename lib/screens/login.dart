ccc522a4d490373992ca97935f8cf97fcd110537
Fareed-Shah <syedfareed667@gmail.com>
2022-05-30 22:59:40 +0500

    f

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -11,21 +11,63 @@ class Login_Screen extends StatefulWidget {
 }
 
 class _Login_ScreenState extends State<Login_Screen> {
+GlobalKey<FormState> formkey = GlobalKey<FormState>();
+String? emailvalidation(email){
+ 
+ if(email!.isEmpty){
+  return "Requierd Email";
+ }
+
+  
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar_widget(),
 
-      body: Column(
-        children: [        
-          Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.all(10),
-            child: TextButton(onPressed: (){}, child: Text_widget(istext: 'Welcome!',txtcolor: Colors.blue)),
-          )
-        
-         
-        ],
+      body: Form(
+        key: formkey,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        child: Column(
+          children: [        
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10),
+              child: TextButton(onPressed: (){}, child: Text_widget(istext: 'Welcome!',txtcolor: Colors.blue)),
+            ),
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10),
+              child: TextButton(onPressed: (){}, child: Text_widget(istext: 'Sign In')),
+            ),
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10),           
+              child: TextFormField(
+                validator: emailvalidation,
+                decoration:  InputDecoration(
+                  prefixIcon: Icon(Icons.mail),
+                  border: OutlineInputBorder(),
+                  labelText: 'User Email'               
+                ),       
+              ),
+            ),
+              Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10),           
+              child: TextFormField(
+                validator: emailvalidation,
+                decoration:  InputDecoration(
+                  prefixIcon: Icon(Icons.mail),
+                  border: OutlineInputBorder(),
+                  labelText: 'User Email'               
+                ),       
+              ),
+            ),
+           
+          ],
+        ),
       ),
       
     );
